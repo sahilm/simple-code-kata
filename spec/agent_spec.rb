62f2ed65753be91e3757d59bb92e4b3ff8b0f812
Sahil Muthoo <sahil.muthoo@gmail.com>
2014-12-21 15:38:48 +0530

    Specify an agent

---
+++ spec/agent_spec.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require_relative '../lib/agent'
+
+describe Agent do
+  it 'should provide its name' do
+    agent = Agent.new(10)
+    agent.name.must_equal(['(sold by agent)'])
+  end
+
+  it "should return the agent's commission when processed" do
+    agent = Agent.new(10)
+    agent.process.must_equal(['Sending 10% commission to the agent.'])
+  end
+end
